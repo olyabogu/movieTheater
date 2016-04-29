ca692bd42d0768f2989831198740181858857c24
Olga_Bogutska <olya_boguckaya@bk.ru>
2016-04-29 12:18:44 +0300

    Add contentNegotiatingViewResolver to configuration

--- src/main/java/com/epam/config/MvcConfiguration.java
+++ src/main/java/com/epam/config/MvcConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
