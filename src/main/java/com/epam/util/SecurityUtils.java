c8a42ffe818e025e96a317c01c225ec81d183690
Olga_Bogutska <olya_boguckaya@bk.ru>
2016-04-29 18:30:25 +0300

    remove unused import

--- src/main/java/com/epam/util/SecurityUtils.java
+++ src/main/java/com/epam/util/SecurityUtils.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class SecurityUtils {
